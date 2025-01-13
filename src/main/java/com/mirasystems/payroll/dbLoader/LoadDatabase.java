25d837fa93dd337aad9ef23c063eb67e1a0a0ac3
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-01-13 03:29:20 -0300

    Create LoadDatabase

---
+++ src/main/java/com/mirasystems/payroll/dbLoader/LoadDatabase.java
@@ -0,0 +1,25 @@
+package com.mirasystems.payroll.dbLoader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mirasystems.payroll.model.Employee;
+import com.mirasystems.payroll.repository.EmployeeRepository;
+
+@Configuration
+public class LoadDatabase {
+
+	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+	
+	@Bean
+	CommandLineRunner initDatabase (EmployeeRepository repository) {
+		
+		return args -> {
+			log.info("Preloading " + repository.save(new Employee("Michael Ribeiro", "Systems Developer Jr")));
+			log.info("Preloading " + repository.save(new Employee("Clark Kent", "Systems Developer Jr")));
+		};
+	}
+}
