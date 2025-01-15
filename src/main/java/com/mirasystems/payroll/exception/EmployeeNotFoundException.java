df5aae2fef5fa58c73d94d9b17845ccf36fd58e8
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-01-14 23:34:40 -0300

    Handler para exceptions

---
+++ src/main/java/com/mirasystems/payroll/exception/EmployeeNotFoundException.java
@@ -0,0 +1,10 @@
+package com.mirasystems.payroll.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = -4331656123393763680L;
+
+	public EmployeeNotFoundException(Long id) {
+		super("Could not find employee " + id);
+	}
+}
