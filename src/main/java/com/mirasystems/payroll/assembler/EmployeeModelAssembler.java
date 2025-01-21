6efbaaff476774b888f57322f37c8d9cc6efffdf
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-01-21 08:00:30 -0300

    feat: simplificar criação de links

---
+++ src/main/java/com/mirasystems/payroll/assembler/EmployeeModelAssembler.java
@@ -0,0 +1,21 @@
+package com.mirasystems.payroll.assembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.mirasystems.payroll.controller.EmployeeController;
+import com.mirasystems.payroll.model.Employee;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+	@Override
+	public EntityModel<Employee> toModel(Employee employee) {
+		return EntityModel.of(employee,
+				linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+				linkTo(methodOn(EmployeeController.class).all()).withRel("eployees"));
+	}
+}
