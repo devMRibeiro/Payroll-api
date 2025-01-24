515244c189b8bd9de862e6c944f98a26e28e7d6a
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-01-23 23:54:15 -0300

    feat: Substituir campo 'name' por 'firstName' e 'lastName'. Adicionar getter 'virtual' para campo 'nome'

--- src/main/java/com/mirasystems/payroll/model/Employee.java
+++ src/main/java/com/mirasystems/payroll/model/Employee.java
@@ -15,16 +15,15 @@ public class Employee {
   @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "employee_id_sequence")
   @SequenceGenerator(name = "employee_id_sequence", sequenceName = "employee_id_sequence", allocationSize = 1)
   private Integer id;
-	
-
-	private String name;
-	
+	private String firstName;
+	private String lastName;
 	private String role;
 
 	public Employee() { }
 
-	public Employee(String name, String role) {
-		this.name = name;
+	public Employee(String firstName, String lastName, String role) {
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.role = role;
 	}
 
@@ -37,11 +36,29 @@ public class Employee {
 	}
 
 	public String getName() {
-		return name;
+		return this.firstName + " " + this.lastName;
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		String[] parts = name.split(" ");
+		this.firstName = parts[0];
+		this.lastName = parts[1];
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 
 	public String getRole() {
@@ -64,12 +81,18 @@ public class Employee {
 		Employee employee = (Employee) o;
 
 		return Objects.equals(this.id, employee.id) &&
-					 Objects.equals(this.name, employee.name) &&
+					 Objects.equals(this.firstName, employee.firstName) &&
+					 Objects.equals(this.lastName, employee.lastName) &&
 					 Objects.equals(this.role, employee.role); 
 	}
 
 	@Override
 	public String toString() {
-		return "Employee{id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+		return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName + '\'' + ", role='" + this.role + '\'' + '}';
   }
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.id, this.firstName, this.lastName, this.role);
+	}
 }
