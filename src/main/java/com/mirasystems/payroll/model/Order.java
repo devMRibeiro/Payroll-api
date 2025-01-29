fc5c410c720dc5e130c41e72cb1f181b771ab89b
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-01-28 23:58:23 -0300

    Adicionar model Order

---
+++ src/main/java/com/mirasystems/payroll/model/Order.java
@@ -0,0 +1,63 @@
+package com.mirasystems.payroll.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY, generator = "order_id_sequence")
+  @SequenceGenerator(name = "order_id_sequence", sequenceName = "order_id_sequence", allocationSize = 1)
+	private Integer id;
+	private String description;
+	private String status;
+	
+	public Order() { }
+	
+	public Order(String description, String status) {
+		this.description = description;
+		this.status = status;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+  public boolean equals(Object o) {
+
+    if (this == o)
+      return true;
+    if (!(o instanceof Order))
+      return false;
+    Order order = (Order) o;
+    return Objects.equals(this.id, order.id) && Objects.equals(this.description, order.description)
+        && this.status == order.status;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.id, this.description, this.status);
+  }
+
+  @Override
+  public String toString() {
+    return "Order{" + "id=" + this.id + ", description='" + this.description + '\'' + ", status=" + this.status + '}';
+  }
+}
