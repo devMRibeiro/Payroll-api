9a96acd6b52063f6604235dfd2cc7d25d47d8cb9
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-01 23:59:49 -0300

    feat: Implementar métodos 'all', 'one' e 'newOrder'

--- src/main/java/com/mirasystems/payroll/controller/OrderController.java
+++ src/main/java/com/mirasystems/payroll/controller/OrderController.java
@@ -1,5 +1,63 @@
 package com.mirasystems.payroll.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mirasystems.payroll.assembler.OrderModelAssembler;
+import com.mirasystems.payroll.enums.EnumStatus;
+import com.mirasystems.payroll.exception.OrderNotFoundException;
+import com.mirasystems.payroll.model.Order;
+import com.mirasystems.payroll.repository.OrderRepository;
+
+@RestController
 public class OrderController {
 
+	private final OrderRepository repository;
+	private final OrderModelAssembler assembler;
+	
+	public OrderController(OrderRepository repository, OrderModelAssembler assembler) {
+		this.repository = repository;
+		this.assembler = assembler;
+	}
+	
+	@GetMapping("/orders")
+	public CollectionModel<EntityModel<Order>> all() {
+		
+		List<EntityModel<Order>> orders = repository.findAll().stream()
+				.map(assembler::toModel)
+				.collect(Collectors.toList());
+		
+		return CollectionModel.of(orders, linkTo(methodOn(OrderController.class).all()).withSelfRel());
+	}
+	
+	@GetMapping("/orders/{id}")
+	public EntityModel<Order> one(@PathVariable Integer id) {
+		Order order = repository.findById(id)
+				.orElseThrow(() -> new OrderNotFoundException(id));
+		
+		return assembler.toModel(order);
+	}
+	
+	@PostMapping("/orders/register")
+	public ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order) {
+		
+		order.setStatus(EnumStatus.IN_PROGRESS);
+		Order newOrder = repository.save(order);
+		
+		return ResponseEntity
+				.created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri())
+				.body(assembler.toModel(newOrder));
+	}
 }
