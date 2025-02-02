f72873e519536e8bc1ec341a7873d00573196b60
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-02 00:01:05 -0300

    feat: Criar Assembler

---
+++ src/main/java/com/mirasystems/payroll/assembler/OrderModelAssembler.java
@@ -0,0 +1,23 @@
+package com.mirasystems.payroll.assembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.mirasystems.payroll.controller.OrderController;
+import com.mirasystems.payroll.model.Order;
+
+@Component
+public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
+
+	@Override
+	public EntityModel<Order> toModel(Order order) {
+		return EntityModel.of(order,
+				linkTo(methodOn(OrderController.class).one(order.getId())).withSelfRel(),
+				linkTo(methodOn(OrderController.class).all()).withRel("order"));
+		
+	}
+}
