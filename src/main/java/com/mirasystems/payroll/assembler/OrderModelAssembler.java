66f97e89621bb5fcdcce23c796829741e65e8765
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-02 21:36:37 -0300

    feat: Adiciona links para 'cancel' e 'complete'

--- src/main/java/com/mirasystems/payroll/assembler/OrderModelAssembler.java
+++ src/main/java/com/mirasystems/payroll/assembler/OrderModelAssembler.java
@@ -8,6 +8,7 @@ import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
 import com.mirasystems.payroll.controller.OrderController;
+import com.mirasystems.payroll.enums.EnumStatus;
 import com.mirasystems.payroll.model.Order;
 
 @Component
@@ -15,9 +16,15 @@ public class OrderModelAssembler implements RepresentationModelAssembler<Order, 
 
 	@Override
 	public EntityModel<Order> toModel(Order order) {
-		return EntityModel.of(order,
+		EntityModel<Order> orderModel = EntityModel.of(order,
 				linkTo(methodOn(OrderController.class).one(order.getId())).withSelfRel(),
 				linkTo(methodOn(OrderController.class).all()).withRel("order"));
 		
+		if (order.getStatus() == EnumStatus.IN_PROGRESS) {
+			orderModel.add(linkTo(methodOn(OrderController.class).cancel(order.getId())).withRel("cancel"));
+			orderModel.add(linkTo(methodOn(OrderController.class).complete(order.getId())).withRel("complete"));
+		}
+		
+		return orderModel;
 	}
 }
