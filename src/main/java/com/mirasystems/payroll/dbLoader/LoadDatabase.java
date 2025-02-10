ff26d0dc6bae1dd09d8edd59cf26190981cdfdad
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-10 03:25:45 -0300

    Inicia aplicação com 1 'Employee' e 2 'Orders'

--- src/main/java/com/mirasystems/payroll/dbLoader/LoadDatabase.java
+++ src/main/java/com/mirasystems/payroll/dbLoader/LoadDatabase.java
@@ -6,8 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.mirasystems.payroll.enums.EnumStatus;
 import com.mirasystems.payroll.model.Employee;
-import com.mirasystems.payroll.repository.EmployeeRepository;
+import com.mirasystems.payroll.model.Order;
+import com.mirasystems.payroll.service.EmployeeService;
+import com.mirasystems.payroll.service.OrderService;
 
 @Configuration
 public class LoadDatabase {
@@ -15,10 +18,17 @@ public class LoadDatabase {
 	private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 	
 	@Bean
-	CommandLineRunner initDatabase (EmployeeRepository repository) {
+	CommandLineRunner initDatabase (EmployeeService employeeService, OrderService orderService) {
 		
 		return args -> {
-			log.info("Preloading " + repository.save(new Employee("Michael", "Ribeiro", "Systems Developer Jr")));
+			log.info("Preloading " + employeeService.save(new Employee("Michael", "Ribeiro", "Systems Developer Jr")));
+			
+			employeeService.getAll().forEach(employee -> log.info("Preloaded: ", employee));
+			
+			orderService.save(new Order("iPhone 13 PRO MAX", EnumStatus.IN_PROGRESS));
+			orderService.save(new Order("Samsung Odyssey Monitor", EnumStatus.COMPLETED));
+			
+			orderService.getAll().forEach(order -> { log.info("Preloaded", order); });
 		};
 	}
 }
